d72f905c41c364f48bd96e666bc9f11f5264cac5
Jorge Alia <jorgealia94@gmail.com>
2022-07-18 18:30:37 +0200

    Add test loginFomr

--- src/components/form/loginForm.tsx
+++ src/components/form/loginForm.tsx
@@ -45,6 +45,7 @@ export function LoginForm() {
                 <input
                     className="input"
                     type="text"
+                    alt="email"
                     name="email"
                     value={formData.email}
                     onChange={handleChange}
