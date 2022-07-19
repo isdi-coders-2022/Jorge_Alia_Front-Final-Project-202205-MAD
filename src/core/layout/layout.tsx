57e0ca72380f498ae5eaacff14298579a0116a25
Jorge Alia <jorgealia94@gmail.com>
2022-07-19 09:43:56 +0200

    Update test layaout

--- src/core/layout/layout.tsx
+++ src/core/layout/layout.tsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import { ReactElement } from 'react';
 import { Footer } from '../footer/footer';
 import { Header } from '../header/header';
 
 export function Layout({ children }: { children: ReactElement }) {
-    return (
+    const template = (
         <>
             <Header></Header>
             <main>{children}</main>
             <Footer></Footer>
         </>
     );
+    return template;
 }
